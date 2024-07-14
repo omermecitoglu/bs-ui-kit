8db72f56612460776703fc39a59f42be6c1e662a
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2024-07-14 19:32:56 +0700

    add ActionButton component

---
+++ src/components/ActionButton.tsx
@@ -0,0 +1,60 @@
+import { faSpinner } from "@fortawesome/free-solid-svg-icons/faSpinner";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import classNames from "classnames";
+import Button from "react-bootstrap/Button";
+import type { IconProp } from "@fortawesome/fontawesome-svg-core";
+import type { ButtonVariant } from "react-bootstrap/types";
+
+type ActionButtonProps = {
+  type?: "button" | "submit" | "reset",
+  text?: string,
+  icon?: IconProp,
+  variant?: ButtonVariant,
+  size?: "sm" | "lg",
+  onClick?: () => void,
+  disabled?: boolean,
+  spinning?: boolean,
+  className?: string,
+  stretched?: boolean,
+};
+
+const ActionButton = ({
+  type = "button",
+  text,
+  icon,
+  variant = "primary",
+  size,
+  onClick,
+  disabled = false,
+  spinning = false,
+  className,
+  stretched = false,
+}: ActionButtonProps) => (
+  <Button
+    type={type}
+    size={size}
+    variant={variant}
+    className={className}
+    onClick={onClick}
+    disabled={disabled || spinning}
+  >
+    {icon ? (
+      <FontAwesomeIcon
+        size="lg"
+        icon={spinning ? faSpinner : icon}
+        className={classNames("fa-fw", {
+          "fa-spin": spinning,
+        })}
+      />
+    ) : (
+      spinning && <FontAwesomeIcon size="lg" icon={faSpinner} className="fa-fw fa-spin" />
+    )}
+    {text && (
+      <span className={icon ? classNames("ms-2", stretched ? "me-4" : "me-2") : undefined}>
+        {text}
+      </span>
+    )}
+  </Button>
+);
+
+export default ActionButton;
