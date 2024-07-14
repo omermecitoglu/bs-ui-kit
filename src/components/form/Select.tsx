921647e99f10c1a5bcba1ad08c3bd7b61015633c
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2024-07-14 19:50:19 +0700

    add form select component

---
+++ src/components/form/Select.tsx
@@ -0,0 +1,41 @@
+import FormSelect from "react-bootstrap/FormSelect";
+import Group from "./Group";
+import Label from "./Label";
+
+type SelectProps<Option> = {
+  label: string,
+  name: string,
+  options: Option[],
+  optionName?: (option: string) => string,
+  defaultValue?: NoInfer<Option>,
+  value?: NoInfer<Option>,
+  onChange?: (value: NoInfer<Option>) => void,
+};
+
+const Select = <Option extends string>({
+  label,
+  name,
+  options,
+  optionName = (option => option),
+  defaultValue,
+  value,
+  onChange,
+}: SelectProps<Option>) => (
+  <Group id={name}>
+    <Label text={label} />
+    <FormSelect
+      name={name}
+      defaultValue={defaultValue}
+      value={value}
+      onChange={onChange && (e => onChange(e.target.value as Option))}
+    >
+      {options.map(option => (
+        <option key={option} value={option}>
+          {optionName(option)}
+        </option>
+      ))}
+    </FormSelect>
+  </Group>
+);
+
+export default Select;
--- src/components/form/index.ts
+++ src/components/form/index.ts
@@ -1,4 +1,5 @@
 export { default as FormWithState } from "./FormWithState";
 export { default as Group } from "./Group";
 export { default as Label } from "./Label";
+export { default as Select } from "./Select";
 export { default as SubmitButton } from "./SubmitButton";
