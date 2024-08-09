251cd6b48db786300b59f32388ead1402aeeb912
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2024-08-09 11:06:00 +0700

    add registration-form component

---
+++ src/components/auth/RegistrationForm.tsx
@@ -0,0 +1,76 @@
+import FormWithState from "../form/FormWithState";
+import Input from "../form/Input";
+import SubmitButton from "../form/SubmitButton";
+
+type RegistrationFormProps = {
+  title: string,
+  action: (_: unknown, formData: FormData) => Promise<Record<string, string>>,
+  displayNameLabel: string,
+  displayNameKey: string,
+  usernameLabel: string,
+  usernameKey: string,
+  passwordLabel: string,
+  passwordKey: string,
+  submitText: string,
+  terms: string,
+  loginSuggestion: string,
+  commonExceptions?: Record<string, string>,
+  emailExceptions?: Record<string, string>,
+};
+
+const RegistrationForm = ({
+  title,
+  action,
+  displayNameLabel,
+  displayNameKey,
+  usernameLabel,
+  usernameKey,
+  passwordLabel,
+  passwordKey,
+  submitText,
+  terms,
+  loginSuggestion,
+  commonExceptions,
+  emailExceptions,
+}: RegistrationFormProps) => (
+  <>
+    <h1 className="fs-4 card-title fw-bold mb-4">{title}</h1>
+    <FormWithState action={action} messages={commonExceptions}>
+      <Input
+        label={displayNameLabel}
+        type="text"
+        name={displayNameKey}
+        autoComplete="name"
+        required
+        autoFocus
+      />
+      <Input
+        label={usernameLabel}
+        type="email"
+        name={usernameKey}
+        autoComplete="username"
+        placeholder="name@example.com"
+        messages={emailExceptions}
+        required
+      />
+      <Input
+        label={passwordLabel}
+        type="password"
+        name={passwordKey}
+        autoComplete="new-password"
+        required
+      />
+      <p className="form-text text-muted my-0">
+        {terms}
+      </p>
+      <div className="d-flex flex-row-reverse justify-content-between align-items-center">
+        <SubmitButton text={submitText} />
+      </div>
+    </FormWithState>
+    <div className="mt-4 mb-2 form-text text-center">
+      {loginSuggestion}
+    </div>
+  </>
+);
+
+export default RegistrationForm;
--- src/components/auth/index.ts
+++ src/components/auth/index.ts
@@ -1,2 +1,3 @@
 export { default as Layout } from "./Layout";
 export { default as LoginForm } from "./LoginForm";
+export { default as RegistrationForm } from "./RegistrationForm";
