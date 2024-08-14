2ab39df0db79ad4aa4dd860d189a2b81a610f0cd
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2024-08-14 15:47:45 +0700

    add navbar brand component

---
+++ src/components/admin/Brand.tsx
@@ -0,0 +1,25 @@
+import type { LinkProps } from "../../types/link";
+import type { ReactNode } from "react";
+
+type BrandProps = {
+  name: string,
+  logo: ReactNode,
+  link: (props: LinkProps) => JSX.Element,
+  href?: string,
+};
+
+const Brand = ({
+  name,
+  logo,
+  link: Link,
+  href = "/",
+}: BrandProps) => (
+  <Link href={href} className="navbar-brand d-flex align-items-center m-0 p-0">
+    {logo}
+    <div className="d-none d-sm-block mx-2">
+      {name}
+    </div>
+  </Link>
+);
+
+export default Brand;
