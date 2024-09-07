6d269a17e6c35f279909d1d14809a62ca1e24c5e
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2024-09-07 15:29:30 +0700

    add zod utils

---
+++ src/utils/zod.ts
@@ -0,0 +1,20 @@
+import { type ZodIssue, ZodIssueCode } from "zod";
+
+function handleZodIssue(issue: ZodIssue) {
+  switch (issue.code) {
+    case ZodIssueCode.invalid_string:
+      return `invalid_${issue.validation}`;
+    default:
+      return "unknown_error";
+  }
+}
+
+export function bundleErrors(issues: ZodIssue[]) {
+  return issues.reduce((bundle, issue) => {
+    const [location] = issue.path;
+    return {
+      ...bundle,
+      [location]: handleZodIssue(issue).toUpperCase(),
+    };
+  }, {} as Record<string, string>);
+}
