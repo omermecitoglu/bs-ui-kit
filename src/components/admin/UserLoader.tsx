35b251c1f2a6c1846beac791ed4e9e7189436eb1
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2025-08-13 09:43:04 +0700

    fix: handle next.js auth interrupt errors

--- src/components/admin/UserLoader.tsx
+++ src/components/admin/UserLoader.tsx
@@ -15,7 +15,15 @@ const UserLoader = async <UserType extends Record<string, unknown>>({
     const user = await loadUser();
     if (!user) throw new Error("user is null");
     return success(user);
-  } catch {
+  } catch (error) {
+    if (error && typeof error === "object" && "digest" in error) {
+      if (error.digest === "NEXT_HTTP_ERROR_FALLBACK;401") {
+        throw error;
+      }
+      if (error.digest === "NEXT_HTTP_ERROR_FALLBACK;403") {
+        throw error;
+      }
+    }
     return fallback ?? success(null as unknown as UserType);
   }
 };
